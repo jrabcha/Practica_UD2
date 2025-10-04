7e5cdbda12bdd84ccfb2d5be8be952ed8b4322eb
jrabcha <jrabcha@gmail.com>
2025-10-04 18:05:51 +0200

    Listado de usuarios y elección del ganador posterior información por partalla del nombre.

--- pract_2.py
+++ pract_2.py
@@ -25,4 +25,15 @@ for user_id in range(1, 16):
     direccion = dato.address()
     email = dato.email()
     telf = dato.phone_number()
-   
+    usuarios[user_id] = {
+        'nombre': nombre,
+        'direccion': direccion,
+        'email': email,
+        'telf': telf
+    }
+
+print(usuarios)
+
+elegido = (random.randrange(1, 16))
+ganador = usuarios.get(elegido)
+print(f'"El usuario con nombre: {ganador['nombre']} es el afortunado!!')
